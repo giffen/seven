4f627860a86019006c3b430f577844878b7e95cb
Andy Giffen <giffen@gmail.com>
2014-05-29 09:58:55 -0700

    Added the Profiles model to admin

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Profile(models.Model):
 	name = models.CharField(max_length=120)
-	description = models.TextField(null=True, blank=True)
+	location = models.CharField(max_length=120, null=True, blank=True)
 
 	def __unicode__(self):
 		return self.name
